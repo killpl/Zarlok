8b0ffbf37cbb026ce0f44c25264244f8c15d12df
Kasia <k_kotynska@wp.pl>
2014-05-06 22:42:44 +0200

    Projekt MVC
    Podłączenie bazy danych. Stworzenie modułu Kategoria.

---
+++ ASP.NET/Zarlok/Testt/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Testt.MvcApplication" Language="C#" %>
---
+++ ASP.NET/Zarlok/Testt/Models/BaseModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Testt.Models
+{
+    public class BaseModel : IDisposable
+    {
+        protected ZarlokEntities1 db;
+
+        public BaseModel()
+        {
+            db = new ZarlokEntities1();
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
